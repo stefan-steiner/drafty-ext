034072b388b959522274f31dd19f2541c6393ce9
Stefan <ssteiner@berkeley.edu>
2025-07-30 08:50:59 -0700

    remove dist

--- dist/parsers/base-parser.d.ts
+++
@@ -1,8 +0,0 @@
-import { SiteParser, PlayerRow } from '../types';
-export declare abstract class BaseParser implements SiteParser {
-    abstract name: string;
-    abstract canParse(url: string): boolean;
-    abstract getPlayerRows(): PlayerRow[];
-    abstract getAvailableNames(requiredCount: number): Promise<string[]>;
-    abstract getDraftedNames(): Promise<string[]>;
-}
--- dist/parsers/base-parser.ts
+++
@@ -1,9 +0,0 @@
-import { SiteParser, PlayerRow, PlayerData } from '../types';
-
-export abstract class BaseParser implements SiteParser {
-  abstract name: string;
-  abstract canParse(url: string): boolean;
-  abstract getPlayerRows(): PlayerRow[];
-  abstract getAvailableNames(requiredCount: number): Promise<string[]>;
-  abstract getDraftedNames(): Promise<string[]>;
-} 
